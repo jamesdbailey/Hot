cbafddbf84019fbad0cc42cf8e2cda891f544fbf
James Bailey <git@jdb.name>
2022-08-12 16:04:53 -0400

    Add likely temperature sensors to the SMC read sensor list. Use both the IOKit HID sensors and the SMC sensors for all values.

--- ThermALL/Classes/SensorsUpdate.swift
+++ ThermALL/Classes/SensorsUpdate.swift
@@ -4,6 +4,84 @@
 
 import Foundation
 
+let SMCM2TemperatureKeys: [String] = [
+    "TPD0",
+    "TPD1",
+    "TPD2",
+    "TPD3",
+    "TPD4",
+    "TPD5",
+    "TPD6",
+    "TPD7",
+    "TPDX",
+    "Te04",
+    "Te05",
+    "Te06",
+    "Tg0e",
+    "Tg0f",
+    "Tg0m",
+    "Tg0n",
+    "Tg0q",
+    "Tg0r",
+    "Th04",
+    "Th05",
+    "Th06",
+    "Th08",
+    "Th09",
+    "Th0A",
+    "Th0C",
+    "Th0D",
+    "Th0E",
+    "Th0G",
+    "Th0H",
+    "Th0I",
+    "Th0K",
+    "Th0L",
+    "Th0M",
+    "Tp00",
+    "Tp01",
+    "Tp02",
+    "Tp04",
+    "Tp05",
+    "Tp06",
+    "Tp08",
+    "Tp09",
+    "Tp0A",
+    "Tp0C",
+    "Tp0D",
+    "Tp0E",
+    "Tp0a",
+    "Tp0b",
+    "Tp0c",
+    "Tp0e",
+    "Tp0f",
+    "Tp0g",
+    "Tp0i",
+    "Tp0j",
+    "Tp0k",
+    "Tp0m",
+    "Tp0n",
+    "Tp0o",
+    "Tp0q",
+    "Tp0r",
+    "Tp0s",
+    "Ts0K",
+    "Ts0L",
+    "Ts0M",
+    "Ts0O",
+    "Ts0P",
+    "Ts0Q",
+    "Ts0S",
+    "Ts0T",
+    "Ts0U",
+    "Ts0W",
+    "Ts0X",
+    "Ts0Y",
+    "Ts0a",
+    "Ts0b",
+    "Ts0c"
+]
+
 public class SensorsUpdate: NSObject {
     @objc public dynamic var socTemperature: NSNumber?
     @objc public dynamic var sensors: [ String : Double ] = [:]
@@ -15,7 +93,24 @@ public class SensorsUpdate: NSObject {
     func readSensors(smc: SMC, prefix: String) -> Dictionary<String, Double> {
         var dict = [String: Double]()
         var keys = smc.getAllKeys()
-        keys = keys.filter{ $0.hasPrefix(prefix)}
+        keys = keys.filter {
+            $0.hasPrefix(prefix)
+        }
+        keys.forEach {
+            (key: String) in
+            let value = smc.getValue(key)
+            dict[key] = value
+        }
+        
+        return dict
+    }
+
+    func readSensors(smc: SMC) -> Dictionary<String, Double> {
+        var dict = [String: Double]()
+        var keys = smc.getAllKeys()
+        keys = keys.filter {
+            SMCM2TemperatureKeys.contains($0)
+        }
         keys.forEach {
             (key: String) in
             let value = smc.getValue(key)
@@ -33,7 +128,7 @@ public class SensorsUpdate: NSObject {
         let smc = SMC()
 
         return Dictionary( uniqueKeysWithValues:
-            readSensors(smc: smc, prefix: "T").map {
+            readSensors(smc: smc).map {
                 ( $0.key, $0.value )
             }
         )
@@ -57,19 +152,23 @@ public class SensorsUpdate: NSObject {
             
             self.updating = true
             
-            let sensors = self.readHIDTemperatureSensors()
-            let all = sensors.mapValues { $0 }
+            let hidSensors = self.readHIDTemperatureSensors()
+            let smcSensors = self.readSMCTemperatureSensors()
+            let allHID: [String:Double] = hidSensors.mapValues { $0 }
+            var allSMC: [String:Double] = smcSensors.mapValues { $0 }
             var temperature = 0.0
             
-            temperature = all.filter {
+            allSMC.merge(allHID) { (_, current) in current }
+            
+            temperature = allSMC.filter {
                 let key = $0.key.lowercased()
                 return key.hasSuffix( "tcal" ) == false && key.hasSuffix( "tr0z" ) == false
             }.reduce( 0.0 ) { r, v in v.value > r ? v.value : r }
             
             if temperature > 0 {
-                self.sensors = sensors.filter {
+                self.sensors = allSMC.filter {
                     let key = $0.key.lowercased()
-                    return key.hasSuffix( "tcal" ) == false && key.hasSuffix( "tr0z" ) == false
+                    return !key.hasSuffix( "tcal" ) && !key.hasSuffix( "tr0z" )
                 }
                 
                 self.socTemperature = NSNumber( value: temperature )
